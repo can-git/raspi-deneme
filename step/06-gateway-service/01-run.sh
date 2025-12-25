2e218e7d3d896ca126fafd587d4ab1fcc09a7417
Can <ist.canyilmaz@gmail.com>
2025-12-25 21:02:31 +0300

    Update 01-run.sh

--- step/06-gateway-service/01-run.sh
+++ step/06-gateway-service/01-run.sh
@@ -15,9 +15,6 @@ install -m 644 files/wirepasGatewayUpdate.service	"${ROOTFS_DIR}/etc/systemd/sys
 echo "Add systemd service to configure sink at boot time"
 install -m 644 files/wirepasSinkConfigurator.service	"${ROOTFS_DIR}/etc/systemd/system/"
 
-echo "Add script to preload images without docker installed"
-install -m 755 files/download-frozen-image-v2.sh  "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
-
 echo "Add mosquitto config to accept ws connection"
 install -m 644 files/config_ws_tcp.conf	"${ROOTFS_DIR}/etc/mosquitto/conf.d/"
 
@@ -27,20 +24,16 @@ systemctl enable wirepasGatewayUpdate.service
 
 systemctl enable wirepasSinkConfigurator.service
 
-cd /home/${FIRST_USER_NAME}
 # Download all images to speed up first boot and generate tar file out of it
-./download-frozen-image-v2.sh transport_service wirepas/gateway_transport_service:latest
-tar -cC 'transport_service' -f transport_service.tar .
-rm -rf transport_service
+cd /home/${FIRST_USER_NAME}
 
-./download-frozen-image-v2.sh sink_service wirepas/gateway_sink_service:latest
-tar -cC 'sink_service' -f sink_service.tar .
-rm -rf sink_service
+docker pull wirepas/gateway_transport_service:latest
+docker save wirepas/gateway_transport_service:latest -o transport_service.tar
 
-./download-frozen-image-v2.sh dbus_service wirepas/gateway_dbus_service:latest
-tar -cC 'dbus_service' -f dbus_service.tar .
-rm -rf dbus_service
+docker pull wirepas/gateway_sink_service:latest
+docker save wirepas/gateway_sink_service:latest -o sink_service.tar
 
-rm download-frozen-image-v2.sh
+docker pull wirepas/gateway_dbus_service:latest
+docker save wirepas/gateway_dbus_service:latest -o dbus_service.tar
 
 EOF
