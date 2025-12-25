db665e37b41d480e2c1d055c344473a7c72fc4c7
Can <ist.canyilmaz@gmail.com>
2025-12-25 22:45:23 +0300

    Update Wi-Fi config installation path in script

--- step/07-wifi/01-run.sh
+++ step/07-wifi/01-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-echo "Installing Wi-Fi config into /boot/wpa_supplicant.conf"
+echo "Installing Wi-Fi config into /boot/firmware/wpa_supplicant.conf"
 
 # use envsubst to substitute variables
 envsubst < files/wpa_supplicant.conf.template \
-    > "${ROOTFS_DIR}/boot/wpa_supplicant.conf"
+    > "${ROOTFS_DIR}/boot/firmware/wpa_supplicant.conf"
 
-chmod 600 "${ROOTFS_DIR}/boot/wpa_supplicant.conf"
+chmod 600 "${ROOTFS_DIR}/boot/firmware/wpa_supplicant.conf"
 
